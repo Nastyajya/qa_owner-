8d944cd47bfa0132bebf7f6893ac2ddc89dd5af0
Nastya <kolod.02@mail.ru>
2023-08-09 23:43:25 +0700

    fix TestBase

--- src/test/java/qa/owner/GitHubTest.java
+++ src/test/java/qa/owner/GitHubTest.java
@@ -1,17 +1,23 @@
 package qa.owner;
 
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
+import qa.owner.config.WebConfig;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+
 public class GitHubTest extends TestBase {
+
 
     @Test
     void TestSolutions() {
+        WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
         step("open page", () -> {
-            open("https://github.com/");
+            open(config.baseUrl());
         });
         step("point to Solutions and click", () -> {
             $(byTagAndText("button", "Solutions")).hover();
--- src/test/java/qa/owner/TestBase.java
+++ src/test/java/qa/owner/TestBase.java
@@ -3,9 +3,11 @@ package qa.owner;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import qa.owner.config.WebConfig;
 import qa.owner.helpers.Attach;
 
 import java.util.Map;
@@ -13,23 +15,18 @@ import java.util.Map;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+    static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
     @BeforeAll
     static void beforeAll() {
         Configuration.pageLoadStrategy = "eager";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://github.com/");
-        Configuration.browser =  System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = config.baseUrl();
+        Configuration.browser =  config.browser();
+        Configuration.browserVersion = config.browserVersion();
+        Configuration.browserSize = config.browserSize();
+        if (config.isRemote()) {
+            Configuration.remote = config.remoteUrl();
+        }
     }
-
-
 
     @AfterEach
      void addAttachments() {
